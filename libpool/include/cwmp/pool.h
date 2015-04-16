f338e961e454fd0e176e57e7254b673fef9fabdf
肥老大 <azhenglive@gmail.com>
2015-04-16 11:00:19 +0800

    Update pool.h
    change pool min size 256=>128

--- libpool/include/cwmp/pool.h
+++ libpool/include/cwmp/pool.h
@@ -35,7 +35,7 @@
 
 #define POOL_MAX_ALLOC_FROM_POOL  (1024 - 1)
 #define POOL_DEFAULT_SIZE    (1024)
-#define POOL_MIN_SIZE        	(256)
+#define POOL_MIN_SIZE        	(128)
 
 
 typedef struct pool_t  pool_t;
